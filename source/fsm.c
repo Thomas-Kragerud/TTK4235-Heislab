ff4e55033ec1edcdaa10dce7bfd207bda7aec867
Kristoffer von Brandis <kristovb@stud.ntnu.no>
2022-02-22 18:54:24 +0100

    ossy stuka lenge

--- source/fsm.c
+++ source/fsm.c
@@ -23,12 +23,12 @@ void fsm_update_light(int floor_indicator){
 }
 
 int fsm_get_next_stop(int floor_indicator){
-    int next_stop = 0;
+    int next_stop = 5;
 
     if(current_direction == DIRECTION_UP) {
         for (int i = floor_indicator; i < NUMBER_OF_FLOORS; ++i) {
             for (int j = 0; j < NUMBER_OF_BUTTONS; ++j) {
-                if ((queue[i][j] == 1) && (i > floor_indicator)/** && (abs(i-floor_indicator) < abs(next_stop-floor_indicator))**/) {
+                if ((queue[i][j] == 1) && (i > floor_indicator) && i != next_stop && (abs(i-floor_indicator) < abs(next_stop-floor_indicator))) {
                     next_stop = i;
                 }
             }
@@ -38,7 +38,7 @@ int fsm_get_next_stop(int floor_indicator){
     if(current_direction == DIRECTION_DOWN) {
         for (int i = 0; i < floor_indicator; ++i) {
             for (int j = 0; j < NUMBER_OF_BUTTONS; ++j) {
-                if ((queue[i][j] == 1) && (i < floor_indicator)/** && (abs(i-floor_indicator) < abs(next_stop-floor_indicator))**/) {
+                if ((queue[i][j] == 1) && (i < floor_indicator) && i != next_stop && (abs(i-floor_indicator) < abs(next_stop-floor_indicator))) {
                     next_stop = i;
                 }
             }
@@ -49,11 +49,15 @@ int fsm_get_next_stop(int floor_indicator){
 }
 
 void fsm_go_to(int next_stop, int floor_indicator) {
-    if (next_stop > floor_indicator) {
-        elevio_motorDirection(DIRN_UP);
-    }
-    if (next_stop < floor_indicator) {
-        elevio_motorDirection(DIRN_DOWN);
+    if (next_stop != 5) {
+        if (next_stop > floor_indicator) {
+            elevio_motorDirection(DIRN_UP);
+        }
+        if (next_stop < floor_indicator) {
+            elevio_motorDirection(DIRN_DOWN);
+        }
+    } else {
+        elevio_motorDirection(DIRN_STOP);
     }
 }
 
@@ -72,6 +76,7 @@ void fsm_run(){
 
     switch(current_state) 
     {
+
         case(INIT):
         if(current_position != 0) {
             elevio_motorDirection(DIRN_DOWN);
@@ -83,30 +88,35 @@ void fsm_run(){
         break;
 
         case(IDLE):
-        while(1) {
-            queue_update_queue();
 
-            current_position = elevio_floorSensor();
-            fsm_update_light(fsm_get_floor_indicator(current_position));
+        printf("IDLE\n");
 
-            int floor_indicator = fsm_get_floor_indicator(current_position);
-            int next_stop = fsm_get_next_stop(floor_indicator);
-            printf("%d", next_stop);
-            printf("%d\n", floor_indicator);
-            if (next_stop != floor_indicator) {
-                fsm_go_to(next_stop, floor_indicator);
-            } else {
-                elevio_motorDirection(DIRN_STOP);
-                fsm_clear_orders_on_floor(next_stop);
-                current_state = DOOR_OPEN;
-                break;
-            }
+        queue_update_queue();
+
+        current_position = elevio_floorSensor();
+        fsm_update_light(fsm_get_floor_indicator(current_position));
+
+        int floor_indicator = fsm_get_floor_indicator(current_position);
+        int next_stop = fsm_get_next_stop(floor_indicator);
+
+        fsm_go_to(next_stop, floor_indicator);
+
+        if (next_stop == floor_indicator) {
+            elevio_motorDirection(DIRN_STOP);
+            fsm_clear_orders_on_floor(next_stop);
+            current_state = DOOR_OPEN;
         }
+
+        printf("%d", next_stop);
+        printf("%d\n", floor_indicator);
+
         break;
 
+
         case(DOOR_OPEN):
-        time_countdown(3);
-        current_state = IDLE;
+        while(1) {
+            int a = 1;
+        }
         break;
     }
 
