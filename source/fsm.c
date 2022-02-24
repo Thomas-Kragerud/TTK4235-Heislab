d4212868919b2044d2660c14ee6eb714a12fbdc9
Maria <mariausy@stud.ntnu.no>
2022-02-24 16:45:49 +0100

    Oskar tester hissen

--- source/fsm.c
+++ source/fsm.c
@@ -16,6 +16,8 @@ movement movement_room;
 
 position next_stop;
 
+int joker = 0;
+
 int fsm_init(void)
 {
     if (current_position != first)
@@ -63,6 +65,18 @@ void fsm_go_to(int next_stop)
         elevio_motorDirection(DIRN_DOWN);
     }
 
+    //LAGT til av oskar for å løse problem 1
+    if (next_stop == newest_floor_position && current_position == -1 && previous_direction == DIRECTION_UP) {
+        elevio_motorDirection(DIRN_DOWN);
+        current_direction = DIRECTION_DOWN;
+        next_stop = newest_floor_position + 1;
+    }
+    if (next_stop == newest_floor_position && current_position == -1 && previous_direction == DIRECTION_DOWN) {
+        elevio_motorDirection(DIRN_UP);
+        current_direction = DIRECTION_UP;
+        next_stop = newest_floor_position - 1;
+    }
+
 }
 
 //finner retningen 
@@ -73,9 +87,13 @@ void fsm_find_directon()
     {
         current_direction = DIRECTION_UP;
     }
-    else
+    if (next_stop < newest_floor_position) 
     {
         current_direction = DIRECTION_DOWN;
+    }
+    if (next_stop == newest_floor_position)
+    {
+        current_direction = DIRECTION_TBT;
     }
 }
 
@@ -119,7 +137,7 @@ void fsm_search_beyond_next_stop()
 
 int fsm_valid_stop()
 {
-    if (queue[newest_floor_position][current_direction] == 1 || queue[newest_floor_position][2] == 1)
+    if ((queue[newest_floor_position][current_direction] == 1 || queue[newest_floor_position][2] == 1) && current_position != -1)
     {
         return 1;
     }
@@ -131,6 +149,28 @@ int fsm_valid_stop()
 
 void fsm_run()
 {
+    
+    if (current_direction == DIRECTION_DOWN) {
+        printf("GOING DOWN");
+    }
+    if (current_direction == DIRECTION_UP) {
+        printf("GOING UP");
+    }
+    if (current_state == IDLE) {
+        printf("IDLE");
+    }
+    if (current_state == MOVING) {
+        printf("MOVING");
+    }
+    if (current_state == DOOR_OPEN) {
+        printf("DOOR OPEN");
+    }
+    if (current_state == EMERGENCY_STOP) {
+        printf("EMERGENCY_STOP");
+    }
+    
+
+    printf("%d\n", next_stop);
 
     // Updates for every run of loop
     fsm_update_floor_position(); // updated {current_position, newest_floor_position, previous_floor_position}
@@ -165,15 +205,15 @@ void fsm_run()
     case (MOVING):
 
         queue_update_queue();
-        if(elevio_stopButton()){current_state = EMERGENCY_STOP;}
         // Hvis ikke er bestemt
         if (current_direction == DIRECTION_TBT)
         {
             fsm_find_directon(); // gir også "første next_stop"
         }
 
-        // Hvis vi er der vi skal være(ende stop) (et ende stopp, vurdering å sette direction til Undetirmand)
-        if (newest_floor_position == next_stop)
+        // Hvis vi er der vi skal være(ende stop) (et ende stelevio_stopButtonopp, vurdering å sette direction til Undetirmand)
+        
+        if (newest_floor_position == next_stop && current_position != -1) //lagt til en && av oskar
         {
             current_state = DOOR_OPEN;
             current_direction = DIRECTION_TBT;
@@ -191,6 +231,11 @@ void fsm_run()
 
         fsm_go_to(next_stop);
 
+        if(elevio_stopButton()){
+            current_state = EMERGENCY_STOP;
+            previous_direction = current_direction;
+            current_direction = DIRECTION_TBT;}
+
         break;
 
     case (DOOR_OPEN):
@@ -198,15 +243,18 @@ void fsm_run()
         if(elevio_stopButton()){current_state = EMERGENCY_STOP;}
         elevio_motorDirection(DIRN_STOP);
         elevio_doorOpenLamp(1);
-        printf("JEG var her");
+        printf("Door open\n");
         queue_clear_row(newest_floor_position);
 
         timer_start();
-        if(timer_times_up(3) && !elevio_obstruction()){
+
+        //Denne funker ikke helt som den skal -oskar
+        if(timer_times_up(2) && !elevio_obstruction()){ //timer_times_up(3) bruker 4 sekunder..
             current_state = IDLE;
             elevio_doorOpenLamp(0);
         }
 
+        
     
         //timer_sec(3);//denne lytter ikke mens, eller hvis jeg leter i main?
         
@@ -221,15 +269,20 @@ void fsm_run()
         elevio_motorDirection(DIRN_STOP);
         elevio_stopLamp(1);
         queue_clear_all();
-        current_direction = DIRECTION_TBT;
+
+
+        //endret av Oskar 
         if(current_position == newest_floor_position)//only true when the elevator is located in a floor
         {
-            elevio_doorOpenLamp(1);
-            if(!elevio_stopButton()){
-                current_state = DOOR_OPEN;
+            if(!elevio_stopButton()) {
                 elevio_stopLamp(0);
-            }
+                current_state = DOOR_OPEN;
+            } else {
+                printf("Door open\n");
+            }      //Eneste som suger er at stoppknappen dimmer...
         } else{
+
+            while(elevio_stopButton()){}
             current_state = IDLE;
             elevio_stopLamp(0);
         }
@@ -237,3 +290,9 @@ void fsm_run()
         break;
     }
 }
+
+
+//Problems::
+
+//1 Etter stopp mellom to etasjer vil ikke heisen registrere trykk på etasjen den akkurat var i
+//2 Ved nødstopp i etasje dimmer stoppknapp lyset veldig pga loopen
